8f75a528f938cd6e8f61f7ad923e975eb5b9149f
Opilane TTHK <opilane@tthk.ee>
2021-09-21 11:51:58 +0300

    kupsis

---
+++ KüpsiseKook/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace KüpsiseKook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mitu küpsist lai on kook?");
+            int Wide = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Mitu küpsist pikk on kook?");
+            int Long = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Mitu kihti on koogil?");
+            int Layer = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kui palju küpsiseid on pakkis?");
+            int Pack = Convert.ToInt32(Console.ReadLine());
+            int Area = Long * Wide;
+            int All = Layer * Area;
+            int CAll = Convert.ToInt32(Math.Round((double)All / Pack));
+            Console.WriteLine($"Sul on vaja {CAll} pakki küpsiseid");
+        }
+    }
+}
